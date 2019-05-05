6e25d313a8f34ba5de9c2fda6fe7e476ccbca812
Katsee <ghtjd000129@naver.com>
2019-05-05 18:32:38 +0900

    첫 커밋

---
+++ Assets/Scripts/ClientManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ClientManager : MonoBehaviour
+{
+    private static ClientManager instance = null;
+    public static ClientManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    TcpClient client;
+    NetworkStream stream;
+    StreamReader reader;
+    StreamWriter writer;
+    bool connected = false;
+
+    void ConnectToServer()
+    {
+        if (connected)
+            return;
+
+        Debug.Log("Trying to Connect to Server...");
+
+        try
+        {
+            client = new TcpClient("127.0.0.1", 19900);
+            stream = client.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+
+            connected = true;
+
+            writer.WriteLine("Connected");
+            writer.Flush();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("ErrMsg : " + e);
+        }
+
+    }
+
+    void Update()
+    {
+        if (!connected)
+        {
+            return;
+        }
+
+        if (stream.DataAvailable)
+        {
+            string data = reader.ReadLine();
+
+            if (data != null)
+            {
+                OnIncommingData(data);
+            }
+        }
+    }
+
+    void OnIncommingData(string data)
+    {
+        Debug.Log("Msg : " + data);
+    }
+
+    void SendMsg(string data)
+    {
+        if (!connected)
+            return;
+
+        writer.WriteLine(data);
+        writer.Flush();
+    }
+}
---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ServerManager : MonoBehaviour
+{
+    private static ServerManager instance = null;
+    public static ServerManager Instance { get { return instance; } }
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    List<Client> clients;
+
+    TcpListener server;
+    bool isServerOn;
+    int port = 19900;
+
+    void Start()
+    {
+        Init_Server();
+    }
+
+    void Update()
+    {
+        if (!isServerOn) return;
+
+        try
+        {
+            foreach (var c in clients)
+            {
+                if (!IsConnected(c.client))
+                {
+                    c.client.Close();
+                    clients.Remove(c);
+                }
+                else
+                {
+                    NetworkStream s = c.client.GetStream();
+                    if (s.DataAvailable)
+                    {
+                        StreamReader reader = new StreamReader(s);
+
+                        string data = reader.ReadLine();
+
+                        if (data != null)
+                        {
+                            OnIncommingData(c.client, data);
+                        }
+                    }
+
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("ErrMsg : " + e);
+        }
+
+    }
+
+    void OnIncommingData(TcpClient c, string data)
+    {
+        Debug.Log("Msg : " + data);
+    }
+
+    bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if (c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                {
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        catch
+        {
+            return false;
+        }
+    }
+
+    void Init_Server()
+    {
+        clients = new List<Client>();
+
+        server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        isServerOn = true;
+
+        StartAccept();
+    }
+
+    void StartAccept()
+    {
+        if (isServerOn)
+            server.BeginAcceptTcpClient(AcceptClient, server);
+    }
+
+    void AcceptClient(IAsyncResult ar)
+    {
+        TcpListener server = (TcpListener)ar.AsyncState;
+
+        clients.Add(new Client(server.EndAcceptTcpClient(ar)));
+        StartAccept();
+    }
+}
+
+
+public class Client
+{
+    public TcpClient client;
+
+    public Client(TcpClient _client)
+    {
+        client = _client;
+    }
+
+
+}
